9329e8f9c37dc23ec0e4f5261944d56df1a9630e
Ivanm1903 <martinivan120@gmail.com>
2021-01-27 16:10:46 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+(function($){
+    // variables
+    elementWidth = $('ul').width(),
+    containerWidth = $('nav').width(),
+    difference = elementWidth-containerWidth,
+    finalWidth = difference * 1.5,
+    element = $('ul');
+    
+    // active on click
+    $('li').on('click', function(){
+      $('li').removeClass('active');
+      $(this).addClass('active');
+    });
+    
+  })(jQuery);
+
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+});
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('.nav').addClass('affix');
+    } else {
+        $('.nav').removeClass('affix');
+    }
+});
+
+
