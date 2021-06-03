25fcb5138e09d3923283d7b4e03cf87c7c9f2542
ivanm1903 <martinivan120@gmail.com>
2021-06-03 18:12:12 +0200

    Navbar-finished

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Logo from "../images/logo.png";
+//REACT FONTAWESOME IMPORTS
+
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#"><img src={Logo} className="logo" alt="logo..." /></a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">HOME <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">ABOUT ME</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">SKILLSET</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">PORTFOLIO</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">CONTACT</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
